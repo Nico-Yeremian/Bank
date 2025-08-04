90f23d75b66170c48ae03023f264359392358f66
pranav2008 <pranavdude2008@gmail.com>
2025-08-04 16:07:45 -0700

    added withdraw and deposit functions

--- Bank.py
+++ Bank.py
@@ -1,4 +1,14 @@
 class Bank:
-    def __init__(self, name, balance):
+    def __init__(self, name):
         self.username = name
-        self.balance = balance
+        self.balance = 100
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+ChaseBank = Bank("Chase Bank")
+ChaseBank.deposit(int(input("How much would you like to deposit? ")))
+ChaseBank.withdraw(int(input("How much would you like to withdraw? ")))
